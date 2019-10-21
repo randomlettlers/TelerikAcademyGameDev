912ce7dd542695feec3a1eb23ede7c1ff0c358f8
Daniel Bashev <fakenamebitch@abv.bg>
2019-10-21 16:30:06 +0300

    Added sky texture.

--- Day One - setup/setup/main.js
+++ Day One - setup/setup/main.js
@@ -1,11 +1,11 @@
 const game = new Phaser.Game(600, 600, Phaser.AUTO, '', { preload, create, update });
 
 function preload() {
-
+    game.load.image('sky', '../img/background-sky (2).png')
 }
 
 function create() {
-   
+   game.add.sprite(0,0,'sky')
 }
 
 function update() {
